00c5a5158b1b1e86c29d7949175fe46bcd926d58
melinator95 <milansanders95@gmail.com>
2015-09-29 10:28:41 +0200

    Added stuff for the first time

---
+++ POS Web Interface/WebContent/overview.jsp
@@ -0,0 +1,48 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Overview</title>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="container">
+		<header>
+			<h1>
+				<span>User Management System</span>
+			</h1>
+			<nav>
+				<ul>
+					<li><a href="Controller">Home</a></li>
+					<li id="actual"><a href="Controller?action=overview">Overview</a></li>
+					<li><a href="Controller?action=signUp">Sign up</a></li>
+				</ul>
+			</nav>
+			<h2>Overview</h2>
+
+		</header>
+		<main>
+		<table>	
+			<tr>
+				<th>E-mail</th>
+				<th>First Name</th>
+				<th>Last Name</th>
+			</tr>
+			<c:forEach var="person" items="${persons}">
+				<tr>
+				<td>${person.userId}</td>
+				<td>${person.firstName}</td>
+				<td>${person.lastName}</td>
+			</tr>
+			</c:forEach>
+			<caption>Users Overview</caption>
+		</table>
+		</main>
+		<footer> &copy; Webontwikkeling 3, UC Leuven-Limburg </footer>
+	</div>
+	<!-- <p>${persons}</p> -->
+</body>
+</html>
---
+++ POS Web Interface/src/controller/Controller.java
@@ -0,0 +1,77 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.person.Person;
+import domain.person.PersonService;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private PersonService personService;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        personService = new PersonService();
+        personService.addPerson(new Person("milan.sanders@ucll.be", "pw001", "Milan", "Sanders"));
+        personService.addPerson(new Person("wouter.dumoulin@ucll.be", "pw002", "Wouter", "Dumoulin"));
+        personService.addPerson(new Person("senne.malcorps@ucll.be", "pw003", "Senne", "Malcorps"));
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+	
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		action = (action == null) ? "home" : action;
+		switch(action) {
+	
+		case "overview":
+			processOverview(request, response);
+			break;
+		case "signUp": //go to the page with the form
+			request.getRequestDispatcher("register.html").forward(request, response);
+			break;
+		case "register": //This is where the actual registration takes place
+			//init
+			String firstName, lastName, email, password;
+			//grab stuff from request
+			firstName = request.getParameter("first");
+			lastName = request.getParameter("last");
+			email = request.getParameter("mail");
+			password = request.getParameter("passwd");
+			//create, also errors get thrown
+			Person person = new Person(email, password, firstName, lastName);
+			personService.addPerson(person);
+			//go back home by falling through the case, clever huh?
+		case "home":
+			request.getRequestDispatcher("index.html").forward(request, response);
+			break;
+		}
+	}
+	
+	private void processOverview(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Person> persons = personService.getPersons();
+		request.setAttribute("persons", persons);		
+		request.getRequestDispatcher("overview.jsp").forward(request, response);
+	}
+
+}
