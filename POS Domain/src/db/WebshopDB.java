09efabcc5a0fe04ab8c9b339620660969ecdfc69
melinator95 <milansanders95@gmail.com>
2015-10-06 10:50:52 +0200

    FIXED ALL THE THINGS, db now works

--- POS Domain/src/db/WebshopDB.java
+++ POS Domain/src/db/WebshopDB.java
@@ -5,17 +5,25 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+import javax.swing.JOptionPane;
+
 public abstract class WebshopDB {
 
 	public static Connection createConnection() throws SQLException {
 		Properties properties = new Properties();
 		String url = "jdbc:postgresql://gegevensbanken.khleuven.be:51516/2TX32";
-		properties.setProperty("user", "");
-		properties.setProperty("password", "");
+		properties.setProperty("user", JOptionPane.showInputDialog("username"));
+		properties.setProperty("password", JOptionPane.showInputDialog("password"));
 		properties.setProperty("ssl", "true");
 		properties.setProperty("sslfactory",
 				"org.postgresql.ssl.NonValidatingFactory");
-
+		
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		Connection connection = DriverManager.getConnection(url, properties);
 		return connection;
 	}
